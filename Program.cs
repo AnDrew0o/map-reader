ba391a3c4d34e34cdbdee74aa7d6bda3e8bf37cd
AnDrew0o <AnDrew0o@DESKTOP-IDEKQEP>
2021-10-06 14:15:52 +0300

    add converter from .bmp to .map

--- Program.cs
+++ Program.cs
@@ -9,11 +9,13 @@ namespace map_reader
     {
         static void Main(string[] args)
         {
-            string path;
-            //string mapName = "Cryofall";
+            //I don't know how to do it is part better yet
 
-            Console.WriteLine("Enter map folder name:");
+            string path;
+
+            Console.WriteLine("Enter map folder (or .bmp) name:");
             string mapName = Console.ReadLine();
+
             if (Directory.Exists(@$"maps\{mapName}"))
             {
                 path = Directory.GetCurrentDirectory() + @$"\maps\{mapName}";
@@ -23,51 +25,69 @@ namespace map_reader
                 Console.WriteLine("Enter absolute path (without name):");
                 path = Console.ReadLine() + @$"\{mapName}";
             }
-            //string path = @$"C:\Users\AnDrew0o\source\repos\map reader\maps\{mapName}";
 
-            MapToImage(path);
-            MapToImage(path, 1);
-            MapToImage(path, 2);
+            Console.WriteLine("1 - generate .bmp\n2 - generate .map");
+            int key = Int32.Parse(Console.ReadLine());
+
+            switch (key)
+            {
+                case 1:
+                    MapToImage(path);
+                    MapToImage(path, "BiomeMap");
+                    MapToImage(path, "HeightMap");
+                    break;
+                case 2:
+                    ImageToMap(path);
+                    break;
+                default:
+                    Console.WriteLine("Exit");
+                    break;
+            }
+
         }
-        public static void MapToImage(string path, int key = 0)
+        public static void MapToImage(string path, string key = "NormalMap")
         {
             int x = 0;
             int y = 0;
 
             GetXandY(path, ref x, ref y);
 
+            //two Dictionaries:
+            //key-tile pair - generate from header
+            //tile-color pair - matching biomes with color
+
             Dictionary<int, string> keyTilePair = GetKeyTilePair(path);
             Dictionary<string, Color> tileColorPair = new Dictionary<string, Color>
             {
-                ["TileForestTropical"] = Color.FromArgb(0xB8, 0xC3, 0x85),
-                ["TileWaterSea"] = Color.FromArgb(0x43, 0x95, 0xEA),
-                ["TileBarren"] = Color.FromArgb(0xFF, 0xBA, 0x66),
-                ["TileSaltFlats"] = Color.FromArgb(0xA5, 0x8F, 0x73),
-                ["TileLakeShore"] = Color.FromArgb(0xDE, 0xB9, 0x74),
-                ["TileRuins"] = Color.FromArgb(0xAE, 0xA6, 0x9B),
-                ["TileWaterLake"] = Color.FromArgb(0x84, 0xD7, 0xE7),
-                ["TileBeachBoreal"] = Color.FromArgb(0xEE, 0xD5, 0x97),
-                ["TileForestBoreal"] = Color.FromArgb(0x8C, 0xB2, 0x7A),
-                ["TileRoads"] = Color.FromArgb(0x65, 0x77, 0x7C),
-                ["TileRocky"] = Color.FromArgb(0xC8, 0xA8, 0x82),
-                ["TileVolcanic"] = Color.FromArgb(0x8C, 0x80, 0x73),
-                ["TileClay"] = Color.FromArgb(0xE2, 0x88, 0x5C),
-                ["TileWaterSeaBridge"] = Color.FromArgb(0x65, 0x77, 0x7C),
-                ["TileLava"] = Color.FromArgb(0xC0, 0x00, 0x00),
-                ["TileAlien"] = Color.FromArgb(0x53, 0x5C, 0x6E),
-                ["TileMeadows"] = Color.FromArgb(0xA1, 0xBB, 0x44),
-                ["TileBeachTemperate"] = Color.FromArgb(0xEE, 0xD5, 0x97),
-                ["TileForestTemperate"] = Color.FromArgb(0xC5, 0xD6, 0x8F),
-                ["TileSwamp"] = Color.FromArgb(0x94, 0x9F, 0x57),
-                ["TileWaterLakeBridge"] = Color.FromArgb(0x65, 0x77, 0x7C),
-                ["Cliff"] = Color.FromArgb(0x8A, 0x62, 0x32)
+                ["TileForestTropical"] =    Color.FromArgb(0xB8, 0xC3, 0x85),
+                ["TileWaterSea"] =          Color.FromArgb(0x43, 0x95, 0xEA),
+                ["TileBarren"] =            Color.FromArgb(0xFF, 0xBA, 0x66),
+                ["TileSaltFlats"] =         Color.FromArgb(0xA5, 0x8F, 0x73),
+                ["TileLakeShore"] =         Color.FromArgb(0xDE, 0xB9, 0x74),
+                ["TileRuins"] =             Color.FromArgb(0xAE, 0xA6, 0x9B),
+                ["TileWaterLake"] =         Color.FromArgb(0x84, 0xD7, 0xE7),
+                ["TileBeachBoreal"] =       Color.FromArgb(0xEE, 0xD5, 0x97),
+                ["TileForestBoreal"] =      Color.FromArgb(0x8C, 0xB2, 0x7A),
+                ["TileRoads"] =             Color.FromArgb(0x65, 0x77, 0x7C),
+                ["TileRocky"] =             Color.FromArgb(0xC8, 0xA8, 0x82),
+                ["TileVolcanic"] =          Color.FromArgb(0x8C, 0x80, 0x73),
+                ["TileClay"] =              Color.FromArgb(0xE2, 0x88, 0x5C),
+                ["TileWaterSeaBridge"] =    Color.FromArgb(0x65, 0x7C, 0x7C),
+                ["TileLava"] =              Color.FromArgb(0xC0, 0x00, 0x00),
+                ["TileAlien"] =             Color.FromArgb(0x53, 0x5C, 0x6E),
+                ["TileMeadows"] =           Color.FromArgb(0xA1, 0xBB, 0x44),
+                ["TileBeachTemperate"] =    Color.FromArgb(0xEE, 0xD5, 0x97),
+                ["TileForestTemperate"] =   Color.FromArgb(0xC5, 0xD6, 0x8F),
+                ["TileSwamp"] =             Color.FromArgb(0x94, 0x9F, 0x57),
+                ["TileWaterLakeBridge"] =   Color.FromArgb(0x65, 0x78, 0x7C),
+                ["Cliff"] =                 Color.FromArgb(0x8A, 0x62, 0x32)
             };
 
             string pathToDataIndex = $@"{path}\ChunkDataIndex.data";
             FileStream chunkDataIndex = new FileStream(pathToDataIndex, FileMode.Open);
 
-            List<int> offsetX = new List<int>();//{ 0, 0, 0, 0, 10, 10, 10, 10, 20, 20, 20, 20 };
-            List<int> offsetY = new List<int>();//{ 0, 10, 20, 30, 0, 10, 20, 30, 0, 10, 20, 30 };
+            List<int> offsetX = new List<int>(); //{ 0, 0, 0, 0, 10, 10, 10, 10, 20, 20, 20, 20 };
+            List<int> offsetY = new List<int>(); //{ 0, 10, 20, 30, 0, 10, 20, 30, 0, 10, 20, 30 };
 
             GenerateOffset(x, y, ref offsetX, ref offsetY);
 
@@ -91,13 +111,13 @@ namespace map_reader
                     {
                         switch (key)
                         {
-                            case 0:
+                            case "NormalMap": //map image like in game
                                 mapMatrix[i, j] = (chunkArr[byteOffset + 1] & (0x80 | 0x40)) == (0x40) ? tileColorPair["Cliff"] : tileColorPair[keyTilePair[chunkArr[byteOffset]]];
                                 break;
-                            case 1:
+                            case "BiomeMap": //biome map 
                                 mapMatrix[i, j] = tileColorPair[keyTilePair[chunkArr[byteOffset]]];
                                 break;
-                            case 2:
+                            case "HeightMap": //height map
                                 var temp = (chunkArr[byteOffset + 1] & ~(0x80 | 0x40)) * 4;
                                 mapMatrix[i, j] = Color.FromArgb(temp, temp, temp);
                                 break;
@@ -121,6 +141,168 @@ namespace map_reader
 
             bmp.Save(@$"{path}{key}.bmp", System.Drawing.Imaging.ImageFormat.Bmp);
             //bmp.Save(@$"{path}{key}.png", System.Drawing.Imaging.ImageFormat.Png);
+        }
+
+        public static void ImageToMap(string path)
+        {
+            Bitmap mapBiome = new Bitmap($@"{path}BiomeMap.bmp");
+            Bitmap mapHeight = new Bitmap($@"{path}HeightMap.bmp");
+
+            int x = mapBiome.Width;
+            int y = mapBiome.Height;
+
+            byte[,] cliffMatrix = new byte[x, y];
+
+            for (int i = 1; i < x - 1; i++)
+            {
+                for (int j = 1; j < y - 1; j++)
+                {
+                    if (mapHeight.GetPixel(i - 1, j).R == mapHeight.GetPixel(i, j).R + 4 ||
+                        mapHeight.GetPixel(i + 1, j).R == mapHeight.GetPixel(i, j).R + 4 ||
+                        mapHeight.GetPixel(i, j - 1).R == mapHeight.GetPixel(i, j).R + 4 ||
+                        mapHeight.GetPixel(i, j + 1).R == mapHeight.GetPixel(i, j).R + 4 ||
+                        mapHeight.GetPixel(i - 1, j - 1).R == mapHeight.GetPixel(i, j).R + 4 ||
+                        mapHeight.GetPixel(i + 1, j + 1).R == mapHeight.GetPixel(i, j).R + 4 ||
+                        mapHeight.GetPixel(i + 1, j - 1).R == mapHeight.GetPixel(i, j).R + 4 ||
+                        mapHeight.GetPixel(i - 1, j + 1).R == mapHeight.GetPixel(i, j).R + 4)
+
+                    {
+                        cliffMatrix[i, j] = 0x40;
+                    }
+                }
+            }
+
+            Random random = new Random();
+
+            /*for (int j = 0; j < y; j++)
+            {
+                for (int i = 1; i < x - 2; i ++)
+                {
+
+                    if (cliffMatrix[i - 1, j] == 0x40 &&
+                        cliffMatrix[i, j] == 0x40 &&
+                        cliffMatrix[i + 1, j] == 0x40 &&
+                        cliffMatrix[i + 2, j] == 0x40 &&
+
+                        cliffMatrix[i - 2, j] != 0x80 &&
+                        cliffMatrix[i + 3, j] != 0x80)
+                    {
+                        cliffMatrix[i, j] = 0x80;
+                        cliffMatrix[i + 1, j] = 0x80;
+                    }
+                }
+            }*/
+
+            List<int> offsetX = new List<int>();//{ 0, 0, 0, 0, 10, 10, 10, 10, 20, 20, 20, 20 };
+            List<int> offsetY = new List<int>();//{ 0, 10, 20, 30, 0, 10, 20, 30, 0, 10, 20, 30 };
+
+            GenerateOffset(x, y, ref offsetX, ref offsetY);
+
+            Dictionary<Color, string> ColorTilePair = new Dictionary<Color, string>
+            {
+                [Color.FromArgb(0xB8, 0xC3, 0x85)] = "TileForestTropical",
+                [Color.FromArgb(0x43, 0x95, 0xEA)] = "TileWaterSea",
+                [Color.FromArgb(0xFF, 0xBA, 0x66)] = "TileBarren",
+                [Color.FromArgb(0xA5, 0x8F, 0x73)] = "TileSaltFlats",
+                [Color.FromArgb(0xDE, 0xB9, 0x74)] = "TileLakeShore",
+                [Color.FromArgb(0xAE, 0xA6, 0x9B)] = "TileRuins",
+                [Color.FromArgb(0x84, 0xD7, 0xE7)] = "TileWaterLake",
+                [Color.FromArgb(0xEE, 0xD5, 0x97)] = "TileBeachBoreal",
+                [Color.FromArgb(0x8C, 0xB2, 0x7A)] = "TileForestBoreal",
+                [Color.FromArgb(0x65, 0x77, 0x7C)] = "TileRoads",
+                [Color.FromArgb(0xC8, 0xA8, 0x82)] = "TileRocky",
+                [Color.FromArgb(0x8C, 0x80, 0x73)] = "TileVolcanic",
+                [Color.FromArgb(0xE2, 0x88, 0x5C)] = "TileClay",
+                [Color.FromArgb(0x65, 0x7C, 0x7C)] = "TileWaterSeaBridge",
+                [Color.FromArgb(0xC0, 0x00, 0x00)] = "TileLava",
+                [Color.FromArgb(0x53, 0x5C, 0x6E)] = "TileAlien",
+                [Color.FromArgb(0xA1, 0xBB, 0x44)] = "TileMeadows",
+                [Color.FromArgb(0xEE, 0xD5, 0x97)] = "TileBeachTemperate",
+                [Color.FromArgb(0xC5, 0xD6, 0x8F)] = "TileForestTemperate",
+                [Color.FromArgb(0x94, 0x9F, 0x57)] = "TileSwamp",
+                [Color.FromArgb(0x65, 0x78, 0x7C)] = "TileWaterLakeBridge"
+                //[Color.FromArgb(0x8A, 0x62, 0x32)] = "Cliff"
+            };
+
+            Dictionary<string, int> TileKeyPair = new Dictionary<string, int>
+            {
+                ["TileForestTropical"] = 0,
+                ["TileWaterSea"] = 1,
+                ["TileBarren"] = 2,
+                ["TileSaltFlats"] = 3,
+                ["TileLakeShore"] = 4,
+                ["TileRuins"] = 5,
+                ["TileWaterLake"] = 6,
+                ["TileBeachBoreal"] = 7,
+                ["TileForestBoreal"] = 8,
+                ["TileRoads"] = 9,
+                ["TileRocky"] = 10,
+                ["TileVolcanic"] = 11,
+                ["TileClay"] = 12,
+                ["TileWaterSeaBridge"] = 13,
+                ["TileLava"] = 14,
+                ["TileAlien"] = 15,
+                ["TileMeadows"] = 16,
+                ["TileBeachTemperate"] = 17,
+                ["TileForestTemperate"] = 18,
+                ["TileSwamp"] = 19,
+                ["TileWaterLakeBridge"] = 20
+                //["Cliff"] = 21
+            };
+
+            Directory.CreateDirectory(@$"{path}Gen");
+            Directory.CreateDirectory(@$"{path}Gen/Chunks");
+
+            FileStream chunkDataIndex = new FileStream(@$"{path}Gen/ChunkDataIndex.data", FileMode.OpenOrCreate);
+
+            byte[] chunkArr = new byte[200];
+
+            for (int v = 0; v < x * y / 100; v++)
+            {
+                int byteOffset = 0;
+                string chunkString = "";
+
+                for (int i = 0 + offsetX[v]; i < 10 + offsetX[v]; i++)
+                {
+                    for (int j = y - 1 - offsetY[v]; j >= y - 10 - offsetY[v]; j--)
+                    {
+                        chunkArr[byteOffset] = Convert.ToByte(TileKeyPair[ColorTilePair[mapBiome.GetPixel(i, j)]]);
+                        //chunkArr[byteOffset + 1] = Convert.ToByte((mapHeight.GetPixel(i, j).R / 4) | cliffMatrix[i, j]);
+                        chunkArr[byteOffset + 1] = Convert.ToByte((mapHeight.GetPixel(i, j).R / 4));
+                        chunkString += $"{chunkArr[byteOffset]}{chunkArr[byteOffset + 1]}";
+                        byteOffset += 2;
+                    }
+                }
+
+                uint chunkHash = (uint)chunkString.GetHashCode();
+
+                foreach (byte a in BitConverter.GetBytes(chunkHash))
+                {
+                    chunkDataIndex.WriteByte(a);
+                }
+
+                if (!File.Exists(@$"{path}Gen/Chunks/{chunkHash}"))
+                {
+                    File.WriteAllBytes(@$"{path}Gen/Chunks/{chunkHash}", chunkArr);
+                }
+            }
+            chunkDataIndex.Close();
+
+            StreamWriter Header = new StreamWriter(@$"{path}Gen/Header.yaml");
+            Header.WriteLine("SerializerVersion: 1");
+            Header.WriteLine("WorldChunkSize: 10");
+            Header.WriteLine("WorldOffset:");
+            Header.WriteLine("  X: 10000");
+            Header.WriteLine("  Y: 10000");
+            Header.WriteLine("WorldSize:");
+            Header.WriteLine($"  X: {x}");
+            Header.WriteLine($"  Y: {y}");
+            Header.WriteLine("ProtoTileBinding:");
+            foreach (var pair in TileKeyPair)
+            {
+                Header.WriteLine($"  {pair.Value}: AtomicTorch.CBND.CoreMod.Tiles.{pair.Key}");
+            }
+            Header.Close();
         }
 
         public static string GetChunkHash(FileStream chunkDataIndex)
@@ -164,7 +346,6 @@ namespace map_reader
         }
         public static void GetXandY(string path, ref int x, ref int y)
         {
-
             string pathToHeader = @$"{path}\Header.yaml";
             string[] lines = File.ReadAllLines(pathToHeader);
             for (int i = 0; i < lines.Length; i++)
--- README.md
+++ README.md
@@ -1,2 +1,66 @@
 # map-reader
-a program to convert a Cryofall .map file into a picture (.bmp)
+À program for convert a Cryofall .map file into a picture (.bmp)
+or convert two .bmp (one - biome map, another - height map, you need both it) to .map
+
+1. To create a picture from a map, you must to unzip map.
+
+To create a map from the pictures, you must name it NameBiomeMap.bmp(for biome map) and Name2.bmp(for height map) 
+You can set "Name" by yourself. 
+
+2. You can't use any images. 
+For Biome map you must use only this colors:
+	TileForestTropical -    #B8C385
+	TileWaterSea -          #4395EA
+	TileBarren -            #FFBA66
+	TileSaltFlats -         #A58F73
+	TileLakeShore -         #DEB974
+	TileRuins -             #AEA69B
+	TileWaterLake -         #84D7E7
+	TileBeachBoreal -       #EED597
+	TileForestBoreal -      #8CB27A
+	TileRocky -             #C8A882
+	TileVolcanic -          #8C8073
+	TileClay -              #E2885C
+	TileLava -              #C00000
+	TileAlien -             #535C6E
+	TileMeadows -           #A1BB44
+	TileBeachTemperate -    #EED597
+	TileForestTemperate -   #C5D68F
+	TileSwamp -             #949F57
+	TileRoads -             #65777C
+	TileWaterLakeBridge -   #65787C
+	TileWaterSeaBridge -    #657C7C
+
+For Height map it's a bit more difficult
+	The generating of the height map depends on the value of the height byte, and since heights can only be from 1 to 63 (but not all are used),
+	I multiply the value of the byte by 4 (so that the colors are more different, and the value was not more than 255), and fill all 3 channels (RGB) with this value.
+	so you can use colors like (4, 4, 4), (64, 64, 64) or (252, 252, 252)
+	In general, 1 < n < 63. your rgb color is (4 * n, 4 * n, 4 * n)
+	The difference of nearby heights should not be more than 1.
+	For example nearby colors should be (40, 40, 40) and (44, 44, 44).
+	If you don't do it, the Cryofall editor will simply add intermediate heights, but it will not be what you expect.
+
+	I will change it in the future, probably.
+	And I want to make a 2D generator.
+
+3. À little bit about drawing a map in a graphical editor
+	Drawing a height map can be difficult, but you can first draw biomes,
+	and then in a new layer draw the heights attached to the biomes (you can do this with many things, but not with all)
+
+4. CryoFall map data specification (*.map) by ai_enabled:
+
+	The whole world is split into chunks of 10x10 tiles. Each chunk contains 
+	an array of tiles. Each tile is just two bytes: first is the tile prototype 
+	index (for the lookup table ProtoTileBinding in Header.yaml file),second is 
+	the height index and cliff/slope flag (more on this below).
+	Chunk filename is just a hashcode (32-bit unsigned integer). 
+	To determine location for each chunk an index file is used (ChunkDataIndex.data). 
+	It contains an array of chunk hashcodes (32-bit unsigned integer). 
+	So its size is the world size multiplied by 4 bytes. 
+	World size and origin offset are encoded in Header.yaml as well as plenty of other data.
+
+	Format for tile height index—it's a single byte so there are 8 bits:
+	— 2^6 (mask 0x40) determines whether it's a cliff
+	— 2^7 (mask 0x80) determines whether it's a slope
+	— this leaves us with 6 bytes to represent the height level (so there are 2^6 - 1 = 63 height levels).
+
